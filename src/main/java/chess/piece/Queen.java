3f11e76c09262c814cd207f4d2d571ca9bdce369
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-22 15:14:20 +0900

    feat: Queen 이동 기능 구현

--- src/main/java/chess/piece/Queen.java
+++ src/main/java/chess/piece/Queen.java
@@ -2,9 +2,16 @@ package chess.piece;
 
 import chess.ChessBoard;
 import chess.Color;
+import chess.Movement;
 import chess.Position;
+import java.util.Set;
 
 public class Queen extends Piece {
+
+    private static final Set<Movement> MOVEMENTS = Set.of(
+            Movement.UP, Movement.DOWN, Movement.LEFT, Movement.RIGHT,
+            Movement.LEFT_UP, Movement.LEFT_DOWN, Movement.RIGHT_UP, Movement.RIGHT_DOWN
+    );
 
     public Queen(Color color, Position position) {
         super(color, position);
@@ -12,6 +19,21 @@ public class Queen extends Piece {
 
     @Override
     boolean isMovable(ChessBoard chessBoard, Position to) {
+        for (Movement movement : MOVEMENTS) {
+            Position curr = position;
+            while (!curr.equals(to)) {
+                if (!curr.canMove(movement)) {
+                    break;
+                }
+                curr = curr.move(movement);
+                if (chessBoard.isExistPiece(curr)) {
+                    break;
+                }
+            }
+            if (curr.equals(to)) {
+                return true;
+            }
+        }
         return false;
     }
 }
